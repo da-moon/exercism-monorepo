7f61e0cc6ca8e377cc1f06391a639fde59f1360b
da-moon <damoon.azarpazhooh@live.com>
2025-11-22 20:52:43 -0500

    Delete rust directory

--- rust/pythagorean-triplet/tests/pythagorean-triplet.rs
+++
@@ -1,8 +0,0 @@
-extern crate pythagorean_triplet;
-
-#[test]
-fn test_answer() {
-    // println!("{:?}",pythagorean_triplet::find());
-    assert_eq!(pythagorean_triplet::find(), Some(31875000));
-    // assert_eq!(true,true);
-}
